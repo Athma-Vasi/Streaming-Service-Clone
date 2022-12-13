43c82288a475f860e0f277813c9fdbfaaa292ace
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-13 16:22:15 -0700

    Header component skeleton

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 import type { NextPage } from 'next';
 import Head from 'next/head';
-import Image from 'next/image';
+import Header from '../components/Header';
 
 const Home: NextPage = () => {
   return (
@@ -9,6 +9,20 @@ const Home: NextPage = () => {
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
+
+      <Header />
+      <main>
+        {/* Banner */}
+        <section>
+          {/* row */}
+          {/* row */}
+          {/* row */}
+          {/* row */}
+          {/* row */}
+          {/* row */}
+        </section>
+      </main>
+      {/* modal */}
     </div>
   );
 };
