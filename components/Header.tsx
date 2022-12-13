a948c5b862cbd8c8bcfe7e348b250ebf748b7e0d
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-13 16:42:24 -0700

    add header links

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,5 +1,25 @@
 function Header() {
-  return <div>Header</div>;
+  return (
+    <header>
+      <div className="flex item-center space-x-2 md:space-x-10">
+        <img
+          src="https://rb.gy/ulxxee"
+          alt="logo-name"
+          width={100}
+          height={100}
+        />
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My Favs</li>
+        </ul>
+      </div>
+
+      <div></div>
+    </header>
+  );
 }
 
 export default Header;
