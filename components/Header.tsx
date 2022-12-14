1250e2fdd630da3d81276a443a5e4a9560fc9077
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-13 19:35:32 -0700

    add logos to header and make it responsive

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,13 +1,20 @@
+import { BellIcon, MagnifyingGlassIcon } from '@heroicons/react/20/solid';
+import Link from 'next/link';
+import { FaDharmachakra } from 'react-icons/fa';
+import { BiHomeSmile } from 'react-icons/bi';
+import { useState, useEffect } from 'react';
+
 function Header() {
   return (
     <header>
       <div className="flex item-center space-x-2 md:space-x-10">
-        <img
+        {/* <img
           src="https://rb.gy/ulxxee"
           alt="logo-name"
           width={100}
           height={100}
-        />
+        /> */}
+        <FaDharmachakra width={100} height={100} />
         <ul className="hidden space-x-4 md:flex">
           <li className="headerLink">Home</li>
           <li className="headerLink">TV Shows</li>
@@ -17,7 +24,14 @@ function Header() {
         </ul>
       </div>
 
-      <div></div>
+      <div className="flex items-center space-x-4 text-sm font-light">
+        <MagnifyingGlassIcon className="hidden sm:inline h-6 w-6" />
+        <p className="hidden lg:inline">Kids</p>
+        <BellIcon className="h-6 w-6" />
+        <Link href="/account">
+          <BiHomeSmile className="cursor-pointer rounded h-6 w-6" />
+        </Link>
+      </div>
     </header>
   );
 }
