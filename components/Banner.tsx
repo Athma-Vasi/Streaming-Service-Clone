6fcb5f73a68967bb4700d60af3241fa311eace84
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-13 21:20:09 -0700

    modify local env for api_key

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,26 @@
+import Image from 'next/image';
 import React from 'react';
 
 function Banner() {
-  return <div>Banner</div>;
+  return (
+    <div>
+      {/* <div>
+      <Image/>
+    </div> */}
+    </div>
+  );
 }
 
 export default Banner;
+
+//api-key : 2cfd768d6f96636193d6a37959011dab
+
+// API Read Access Token (v4 auth)
+/**
+ eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIyY2ZkNzY4ZDZmOTY2MzYxOTNkNmEzNzk1OTAxMWRhYiIsInN1YiI6IjYzOTkzZTliNmU0NGJmMDA4OTZlOTcxMyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.7EL6JSfhp9sesQIcSyVAgeKUyofAMZV_Wp9o-PKAvLU
+ */
+
+// Example API Request
+/**
+ https://api.themoviedb.org/3/movie/550?api_key=2cfd768d6f96636193d6a37959011dab
+ */
