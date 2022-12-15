eb9d0674722f2c91837a9b6d82127f61f1403842
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-15 15:47:37 -0700

    add scroll functionality to thumbnails

--- components/Row.tsx
+++ components/Row.tsx
@@ -1,7 +1,65 @@
-import React from 'react';
+import { useRef, useState } from 'react';
+import { FaChevronLeft, FaChevronRight } from 'react-icons/fa';
+import { Movie } from '../types';
+import Thumbnail from './Thumbnail';
 
-function Row() {
-  return <div>Row</div>;
+type RowProps = {
+  title: string;
+  //when using firebase
+  // movie: Movie | DocumentData[];
+  movies: Movie[];
+};
+
+function Row({ title, movies }: RowProps) {
+  const rowRef = useRef<HTMLDivElement>(null);
+  const [isMoved, setIsMoved] = useState(false);
+
+  function handleClick(direction: 'left' | 'right') {
+    setIsMoved(true);
+
+    if (rowRef.current) {
+      const { scrollLeft, clientWidth } = rowRef.current;
+      const scrollingTo =
+        direction === 'left'
+          ? scrollLeft - clientWidth
+          : scrollLeft + clientWidth;
+
+      rowRef.current.scrollTo({ left: scrollingTo, behavior: 'smooth' });
+    }
+  }
+
+  console.log('scrollLeft: ', rowRef.current?.scrollLeft);
+  console.log('clientWidth: ', rowRef.current?.clientWidth);
+
+  return (
+    <div className="h-40 space-y-0.5 md:space-y-2">
+      <h2 className="w-56 cursor-pointer text-sm font-semibold text-[#e5e5e5] transition duration-300 hover:text-white md:text-2xl">
+        {title}
+      </h2>
+      <div className="group relative md:-ml-2">
+        <FaChevronLeft
+          className={`absolute top-0 bottom-0 left-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:-scale-y-125 group-hover:opacity-100 ${
+            !isMoved && 'hidden'
+          }`}
+          onClick={() => handleClick('left')}
+        />
+
+        <div
+          ref={rowRef}
+          className="flex items-center space-x-0.5 overflow-x-scroll md:space-x-2.5 md:p-2 scrollbar-hide"
+        >
+          {movies.map((movie) => (
+            <Thumbnail key={movie.id} movie={movie} />
+          ))}
+        </div>
+
+        <FaChevronRight
+          className="absolute top-0 bottom-0 right-2 z-40 m-auto h-9 w-9 cursor-pointer opacity-0 transition hover:-scale-y-125 group-hover:opacity-100"
+          onClick={() => handleClick('right')}
+        />
+      </div>
+    </div>
+  );
 }
 
 export default Row;
