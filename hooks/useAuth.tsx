38a0ae13939b16d25f6e4c14a9b6f019abb510f5
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-16 15:14:08 -0700

    add signUp func using firebase createUserWithEmailAndPassword()

--- hooks/useAuth.tsx
+++ hooks/useAuth.tsx
@@ -1,9 +1,45 @@
-import { useState } from 'react';
+import {
+  createUserWithEmailAndPassword,
+  onAuthStateChanged,
+  signInWithEmailAndPassword,
+  signOut,
+  User,
+} from 'firebase/auth';
 
-function useAuth() {
-  const [user, setUser] = useState();
+import { useRouter } from 'next/router';
+import { createContext, useContext, useEffect, useMemo, useState } from 'react';
+import { auth } from '../firebase';
 
-  return <div>useAuth</div>;
-}
+type AuthProviderProps = {
+  children: React.ReactNode;
+};
+
+export const AuthProvider = ({ children }: AuthProviderProps) => {
+  const [loading, setLoading] = useState(false);
+  //
+  const [user, setUser] = useState<User | null>(null);
+  const router = useRouter();
+
+  // an async sign up function that manages sign up of a new user
+  const signUp = async (email: string, password: string) => {
+    setLoading(true);
+    //from firebase auth that returns a promise
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        //this is a type of User, the type is from firebase
+        setUser(userCredential.user);
+        //from the login screen, after the user is logged in, we push the user into the main page
+        router.push('/');
+        //user is successfully logged in, then change the state
+        setLoading(false);
+      })
+      .catch((error) => alert(error.message))
+      //even if there is an error, modify the loading state to false
+      .finally(() => setLoading(false));
+  };
+
+  //
+  return <AuthContext.Provider>{children}</AuthContext.Provider>;
+};
 
 export default useAuth;
