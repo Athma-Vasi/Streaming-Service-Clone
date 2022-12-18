f750a95535d9c160152e0d83b35922bc05e6ec76
Athma Chidambaravasi <athma.vasi@protonmail.com>
2022-12-17 19:26:15 -0700

    add movie info below react-player modal pop-up

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -82,7 +82,49 @@ function Modal() {
               </button>
             </div>
 
-            <button>{muted ? <FaVolumeOff /> : <FaVolumeUp />}</button>
+            <button className="modalButton" onClick={() => setMuted(!muted)}>
+              {muted ? (
+                <FaVolumeOff className="h-7 w-7" />
+              ) : (
+                <FaVolumeUp className="h-7 w-7" />
+              )}
+            </button>
+          </div>
+        </div>
+
+        <div className="flex space-x-16 rounded-b-md bg-[#181818] px-10 py-8">
+          <div className="space-y-6 text-lg">
+            <div className="flex items-center space-x-2 text-sm">
+              <p className="font-semibold text-green-400">
+                {movie?.vote_average * 10}% match
+              </p>
+              <p className="font-light">
+                {movie?.release_date ?? movie?.first_air_date}
+              </p>
+              <div className="flex h-4 items-center justify-center rounded border border-white/40 px-1.5 text-xs ">
+                HD
+              </div>
+            </div>
+
+            <div className="flex flex-col gap-x-10 gap-y-4 font-light md:flex-row">
+              <p className="w-5/6 py-2">{movie?.overview}</p>
+              <div className="flex flex-col space-y-3 text-sm">
+                <div>
+                  <span className="text-[gray]">Genres: </span>
+                  {genres.map((genre) => genre.name).join(', ')}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Original language: </span>
+                  {movie?.original_language}
+                </div>
+
+                <div>
+                  <span className="text-[gray]">Total votes: </span>
+                  {movie?.vote_count}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
